fed457e49e9421a9414dccc3dbe2e77cdab50265
rmit-jc <31055202+rmit-jc@users.noreply.github.com>
2018-12-19 15:29:05 +1100

    Cleaning up comments

--- app.py
+++ app.py
@@ -15,21 +15,12 @@ gethub = Videos()
 page = 1
 qty = 30
 
-# videos = []
-# for video in gethub.getVideos(page, qty):
-    
-#     print(json.dumps(video, indent=4, sort_keys=True))
-#     videos.append(json.dumps(video, indent=4, sort_keys=True))
-
 @app.route('/', defaults={'page':1}, methods=['GET', 'POST'])
 @app.route('/<int:page>')
 def home(page):
     videos = []
     
     for video in gethub.getVideos(page, qty):
-        # print(json.dumps(video, indent=4, sort_keys=True))
-        # print("\n")
-        #videos.append(json.dumps(video, indent=4, sort_keys=True))
         videos.append(video)
 
  
@@ -42,10 +33,4 @@ def playvideo():
     viewkey = request.args.get('viewkey')
     top_comment = gethub.getComment(gethub.getSingleVideoPage("https://www.pornhub.com/view_video.php?viewkey=" + viewkey))
     
-
     return render_template("playvideo.html", viewkey=viewkey, top_comment=top_comment)
-
-# if __name__ == '__main__':
-#     # This is used when running locally. Gunicorn is used to run the
-#     # application on Google App Engine. See entrypoint in app.yaml.
-#     app.run(host='127.0.0.1', port=8080, debug=True)
